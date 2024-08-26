db8507cd1b4b471a95e9ada4eb38940915eeb5fa
Adnan Semic <148137889+ShemichAdnan@users.noreply.github.com>
2024-08-26 11:11:25 +0200

    Dodana dugmad koja su falila

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let number1;
+let number2;
+let operator;
+const c=document.querySelector("#c");
+const del=document.querySelector("#del");
+const percent=document.querySelector("#percent");
+const slash=document.querySelector("#slash");
+const seven=document.querySelector("#seven");
+const eight=document.querySelector("#eight");
+const nine=document.querySelector("#nine");
+const asterisk=document.querySelector("#asterisk");
+const four=document.querySelector("#four");
+const five=document.querySelector("#five");
+const six=document.querySelector("#six");
+const minus=document.querySelector("#minus");
+const one=document.querySelector("#one");
+const two=document.querySelector("#two");
+const three=document.querySelector("#three");
+const plus=document.querySelector("#plus");
+const zero=document.querySelector("#zero");
+const dot=document.querySelector("#dot");
+const equal=document.querySelector("#equal");
+
+let input=document.querySelector("#input");
+let calculate=document.querySelector("#calculate")
+
