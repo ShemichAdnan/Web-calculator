813d986b1d9337a8e2ed532f9dda89c9a9185247
ShemichAdnan <adnan.semic.23@size.ba>
2025-02-08 23:42:00 +0100

    Zavrsene sve funkcije. Beta test kalkulatora

--- javascript.js
+++ javascript.js
@@ -20,14 +20,12 @@ const plus=document.querySelector("#plus");
 const zero=document.querySelector("#zero");
 const dot=document.querySelector("#dot");
 const equal=document.querySelector("#equal");
+const negation=document.querySelector("#negation");
 
 let input=document.querySelector("#input h2");
 let calculate=document.querySelector("#calculate h2");
 
-let max=10;
-let max1=9999999999;
-let max2=-999999999;
-let temp="";
+
 zero.addEventListener("click",()=>{InputNumber(zero.textContent)});
 one.addEventListener("click",()=>{InputNumber(one.textContent)});
 two.addEventListener("click",()=>{InputNumber(two.textContent)});
@@ -40,16 +38,16 @@ eight.addEventListener("click",()=>{InputNumber(eight.textContent)});
 nine.addEventListener("click",()=>{InputNumber(nine.textContent)});
 
 let maxc=15;
-let max0=999999999999999;
+let max=999999999999999;
 let min=-99999999999999;
 let op;
 let tempOperator;
 
 let ProvjeraDuzine=()=>{
     if(input.textContent.length>9){
-        input.style.fontSize = "35px";
+        input.style.fontSize = "40px";
     }else{
-        input.style.fontSize="40px";
+        input.style.fontSize="50px";
     }
 };
 let Clear=()=>{
@@ -62,6 +60,9 @@ let Clear=()=>{
 };
 let Del=()=>{
     input.textContent=input.textContent.slice(0,input.textContent.length-1)
+    if(input.textContent=="-"){
+        input.textContent="";
+    }
     ProvjeraDuzine();
 };
 let InputNumber=(c)=>{
@@ -70,7 +71,7 @@ let InputNumber=(c)=>{
         ProvjeraDuzine();
     }
 };
-let addDot=()=>{
+let AddDot=()=>{
     if(input.textContent.includes('.')){
         return;
     }else{
@@ -81,148 +82,100 @@ let addDot=()=>{
         ProvjeraDuzine();
     }
 }
+let NumberNegation=()=>{
+    if(input.textContent!=""){
+        input.textContent=input.textContent*-1;
+    }    
+}
+let Calculation=()=>{
+    if(number2=="0" && tempOperator=="/"){
+        Clear();
+        alert("Dijeljenje sa 0 nije moguće!");
+    }else if(number1!=null && number2!=null && number1!="" && number2!=""){
+        let result;
+        if(tempOperator=="+"){
+            result=+number1 + +number2;
+        }else if(tempOperator=="-"){
+            result=+number1 - +number2;
+        }else if(tempOperator=="*"){
+            result=+number1 * +number2;
+        }else if(tempOperator=="/"){
+            result=number1/number2;
+        }else{
+            result=(+number1/100)* +number2;
+        }
+
+        if(result>max || result<min){
+            Clear();
+            alert("Previše velik / previše mal broj!");
+        }
+        else{
+            if(result.toString().length>maxc){
+                temp=result.toString().slice(0,15)
+                input.textContent=temp;
+                ProvjeraDuzine();
+            }else{
+                input.textContent=result;
+                ProvjeraDuzine();
+            }
+            number1="";
+            number2="";
+            tempOperator="";
+        }
+    }
+}
+
+let Operate=(c)=>{
+    op=c;
+    Execution();
+}
+
+let Execution=()=>{
+    if(input.textContent==null || input.textContent==""){
+        
+        input.textContent="0";
+        if(tempOperator=="*" || tempOperator=="/"){
+            input.textContent="1";
+        }else if(tempOperator=="%"){
+            input.textContent="100";
+        }
+    }
+
+    if(number1=="" && op!="="){
+        if(input.textContent[input.textContent.length-1]=="."){
+            number1=input.textContent.slice(0,input.textContent.length-1)
+        }
+        else{
+            number1=input.textContent;
+        }
+
+        input.textContent="";
+        tempOperator=op;
+        calculate.textContent=number1+" "+tempOperator;
+    }
+    else{
+        if(input.textContent[input.textContent.length-1]=="."){
+            number2=input.textContent.slice(0,input.textContent.length-1)
+        }
+        else{
+            number2=input.textContent;
+        }
+        Calculation();
+        if(op!="="){
+            Execution();
+        }else{
+            calculate.textContent="";
+        }
+    }
+}
 
 c.addEventListener("click",Clear);
 d.addEventListener("click",Del);
-plus.addEventListener("click",()=>operate('+'));
-minus.addEventListener("click",()=>operate('-'))
-equal.addEventListener("click",()=>operate('='));
-asterisk.addEventListener("click",()=>operate('*'));
-slash.addEventListener("click",()=>operate('/'));
-percent.addEventListener("click",()=>operate('%'));
-dot.addEventListener("click",addDot);
-
-
-let add=()=>{
-    console.log(`add ${number1} + ${number2}`)
-    if(number1+number2>max1 || number1+number2<max2){
-        alert("Number is to high!")
-        clear();
-    }else{
-        if((number1+number2).toString().length>max){
-            temp=(number1+number2).toString().slice(0,10)
-            input.textContent=+temp;
-        }else{
-            input.textContent=number1+number2;
-        }
-        number1='';
-        number2='';
-        operator='';
-    }
-}
-let subtract=()=>{
-    console.log(`subtract ${number1} - ${number2}`)
-    if(number1-number2>max1 || number1-number2<max2){
-        alert("Number is to high!")
-        clear();
-    }else{
-        if((number1-number2).toString().length>max){
-            temp=(number1-number2).toString().slice(0,10)
-            input.textContent=+temp;
-        }else{
-            input.textContent=number1-number2;
-        }
-        number1='';
-        number2='';
-        operator='';
-    }
-}
-let multiply=()=>{
-    console.log(`multiply ${number1} * ${number2}`)
-    if(number1*number2>max1 || number1*number2<max2){
-        alert("Number is to high!")
-        clear();
-    }else{
-        if((number1*number2).toString().length>max){
-            temp=(number1*number2).toString().slice(0,10)
-            input.textContent=+temp;
-        }else{
-            input.textContent=number1*number2;
-        }
-        number1='';
-        number2='';
-        operator='';
-    }
-}
-let divide=()=>{
-    console.log(`devide ${number1} / ${number2}`)
-    if(number1/number2>max1 || number1/number2<max2){
-        alert("Number is to high!")
-        clear();
-    }else{
-        if((number1/number2).toString().length>max){
-            temp=(number1/number2).toString().slice(0,10);
-            input.textContent=+temp;
-        }else{
-            input.textContent=number1/number2;
-        }
-        number1='';
-        number2='';
-        operator='';  
-    }
-}
-let percentage=()=>{
-    console.log(`percentage ${number1} % ${number2}`)
-    if(number1/100*number2>max1 || number1/100*number2<max2){
-        alert("Number is to high!")
-        clear();
-    }else{
-        if((number1/100*number2).toString().length>max){
-            temp=(number1/100*number2).toString().slice(0,10);
-            input.textContent=+temp;
-        }else{
-            input.textContent=number1/100*number2;
-        }
-        number1='';
-        number2='';
-        operator='';
-    }
-}
-
-let operate=(op)=>{   
-    if(number1=='' && op!='='){
-        number1=+input.textContent;
-        input.textContent='';
-        operator=op;
-        calculate.textContent=number1+" "+operator;
-    }else{
-        number2=+input.textContent;
-
-        if(operator=='+'){
-            add();
-            if(op!='='){
-                operate(op);
-            }else{
-                calculate.textContent='';
-            }
-        }else if(operator=='-'){
-            subtract();
-            if(op!='='){
-                operate(op);
-            }else{
-                calculate.textContent='';
-            }
-        }else if(operator=='*'){
-            multiply();
-            if(op!='='){
-                operate(op);
-            }else{
-                calculate.textContent='';
-            }
-        }else if(operator=='/'){
-            divide();
-            if(op!='='){
-                operate(op);
-            }else{
-                calculate.textContent='';
-            }
-        }else if(operator=='%'){
-            percentage();
-            if(op!='='){
-                operate(op);
-            }else{
-                calculate.textContent='';
-            }
-        }
-    }
-}
+plus.addEventListener("click",()=>Operate('+'));
+minus.addEventListener("click",()=>Operate('-'))
+equal.addEventListener("click",()=>Operate('='));
+asterisk.addEventListener("click",()=>Operate('*'));
+slash.addEventListener("click",()=>Operate('/'));
+percent.addEventListener("click",()=>Operate('%'));
+dot.addEventListener("click",AddDot);
+negation.addEventListener("click",NumberNegation);
